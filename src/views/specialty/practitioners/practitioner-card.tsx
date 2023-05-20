5ac58cd0057b5e2aed379d33e37098e65528c55a
Giorgio Zanni <g.zanni@hotmail.com>
2023-05-20 19:26:14 -0400

    feat: update practitioner card to match new design

--- src/views/specialty/practitioners/practitioner-card.tsx
+++ src/views/specialty/practitioners/practitioner-card.tsx
@@ -2,23 +2,22 @@ import { getComponentClassNames } from "@marketplace/ui/namespace";
 import Image from "next/image";
 import Link from "next/link";
 import { Practitioner } from "./types";
+import { Icon } from "@marketplace/ui/icon";
 
 export type PractitionerCardProps = Practitioner;
 
 const classes = getComponentClassNames("practitioner-card", {
-  link: 'link',
-  imageContainer: 'image-container',
-  image: 'image',
-  content: 'content',
-  title: 'title',
-  specialty: 'specialty',
-  addressesContainer: 'addresses-container',
-  addressesTitle: 'addresses-title',
-  addresses: 'addresses',
-  address: 'address',
-  action: 'action',
-  actionLabel: 'action-label',
-  actionIcon: 'action-icon',
+  link: "link",
+  imageContainer: "image-container",
+  image: "image",
+  content: "content",
+  title: "title",
+  specialty: "specialty",
+  addressesContainer: "addresses-container",
+  addresses: "addresses",
+  address: "address",
+  addressIcon: "address-icon",
+  action: "action",
 });
 
 export const PractitionerCard = ({
@@ -31,21 +30,31 @@ export const PractitionerCard = ({
   <li className={classes.namespace}>
     <Link href={`/profesional/${code}`} className={classes.link}>
       <div className={classes.imageContainer}>
-        <Image src={picture} alt={name} height="100" width="100" className={classes.image} />
+        <Image
+          src={picture}
+          alt={name}
+          height="100"
+          width="100"
+          className={classes.image}
+        />
       </div>
       <div className={classes.content}>
         <h3 className={classes.title}>{name}</h3>
-        <div className={classes.specialty}>
-          {specialty}
-        </div>
+        <div className={classes.specialty}>{specialty}</div>
         <div className={classes.addressesContainer}>
-          <div className={classes.addressesTitle}>Lugar de atención:</div>
           <div className={classes.addresses}>
-            {addressTags.map(tag => <div key={`address-${code}-${tag}`} className={classes.address}>{tag}</div>)}
+            {addressTags.map((tag) => (
+              <div key={`address-${code}-${tag}`} className={classes.address}>
+                <span className={classes.addressIcon}>
+                  <Icon id="map-location-dot" variant="solid" />
+                </span>
+                {tag}
+              </div>
+            ))}
           </div>
         </div>
       </div>
-      <button className={classes.action}><span className={classes.actionLabel}>Ver sobrecupos</span><span className={classes.actionIcon}>-&gt;</span></button>
+      <button className={classes.action}>Ver sobrecupos</button>
     </Link>
   </li>
 );
