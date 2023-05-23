8637c77632a4b26415652a8a1d2cbc7adfc81b1c
Giorgio Zanni <g.zanni@hotmail.com>
2023-05-22 21:52:03 -0400

    feat: add new about us section

--- src/views/main/about-us/about-us.tsx
+++ src/views/main/about-us/about-us.tsx
@@ -1,34 +1,78 @@
+import { Icon } from "@marketplace/ui/icon";
 import { getComponentClassNames } from "@marketplace/ui/namespace";
+import classNames from "classnames";
 import Image from "next/image";
 
 const classes = getComponentClassNames("about-us", {
-  imageContainer: 'image-container',
+  imageContainer: "image-container",
   image: "image",
   content: "content",
+  arrow: "arrow",
+  card: "card",
   title: "title",
   text: "text",
-  highlight: 'highlight',
+  highlight: "highlight",
 });
 
 export const AboutUs = () => (
   <div className={classes.namespace}>
-    <div className={classes.imageContainer}>
-      <Image
-        src="/about-us.webp"
-        alt="Persona meditando"
-        width="363"
-        height="281"
-        className={classes.image}
-      />
-    </div>
+    <h5 className={classes.title}>Fácil de usar</h5>
     <div className={classes.content}>
-      <h5 className={classes.title}>¿Qué es un <span className={classes.highlight}>sobrecupo</span>?</h5>
-      <p className={classes.text}>
-        Imagina poder atender tu problema de salud en el mismo día, sin tener
-        que esperar semanas o incluso meses por una cita. Con{" "}
-        <strong>sobrecupos</strong>, tendrás acceso prioritario a la atención
-        médica que necesitas, sin sacrificar la calidad de la atención.
-      </p>
+      <div className={classes.card}>
+        <div className={classes.imageContainer}>
+          <Image
+            src="/about-us-1.webp"
+            alt="Persona meditando"
+            width="191"
+            height="191"
+            className={classes.image}
+          />
+        </div>
+        <p className={classes.text}>
+          Selecciona y toma la{" "}
+          <span className={classes.highlight}>
+            autorización de tu sobrecupo
+          </span>
+        </p>
+      </div>
+      <div className={classNames(classes.arrow, classes.highlight)}>
+        <Icon id="arrow-down" variant="solid" />
+        <Icon id="arrow-right" variant="solid" />
+      </div>
+      <div className={classes.card}>
+        <div className={classes.imageContainer}>
+          <Image
+            src="/about-us-2.webp"
+            alt="Persona meditando"
+            width="191"
+            height="191"
+            className={classes.image}
+          />
+        </div>
+        <p className={classes.text}>
+          <span className={classes.highlight}>Acude al centro médico</span> y
+          paga tu consulta
+        </p>
+      </div>
+      <div className={classNames(classes.arrow, classes.highlight)}>
+        <Icon id="arrow-down" variant="solid" />
+        <Icon id="arrow-right" variant="solid" />
+      </div>
+      <div className={classes.card}>
+        <div className={classes.imageContainer}>
+          <Image
+            src="/about-us-3.webp"
+            alt="Persona meditando"
+            width="191"
+            height="191"
+            className={classes.image}
+          />
+        </div>
+        <p className={classes.text}>
+          Resuelve tu problema de salud{" "}
+          <span className={classes.highlight}>el mismo día</span>
+        </p>
+      </div>
     </div>
   </div>
 );
