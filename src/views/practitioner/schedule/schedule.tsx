110570d1447434772a0aac8eb4c0df53cec22516
Giorgio Zanni <g.zanni@hotmail.com>
2023-07-10 09:48:09 -0400

    fix: ensure timezone is consistent

--- src/views/practitioner/schedule/schedule.tsx
+++ src/views/practitioner/schedule/schedule.tsx
@@ -46,6 +46,7 @@ const classes = getComponentClassNames("schedule", {
 const formatDate = (dateString: string) => {
   const formattedDate = new Intl.DateTimeFormat("es-CL", {
     dateStyle: "full",
+    timeZone: 'America/Santiago'
   }).format(new Date(dateString));
 
   return formattedDate[0].toUpperCase() + formattedDate.slice(1);
