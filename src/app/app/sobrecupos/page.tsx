497a09c0ffd0c0a29996bee2f84ba352d41802f4
Giorgio Zanni <g.zanni@hotmail.com>
2023-10-02 12:42:18 -0300

    feat: update appointments page

--- src/app/app/sobrecupos/page.tsx
+++ src/app/app/sobrecupos/page.tsx
@@ -1,17 +1,8 @@
-import { appointmentsService } from "@marketplace/data-access/appointments/appointments.service";
 import { authService } from "@marketplace/data-access/auth/auth.service";
 import { practitionersService } from "@marketplace/data-access/practitioners/practitioners.service";
-import { AppointmentsForm } from "@marketplace/features/appointments-form";
-import { Card } from "@marketplace/ui/card";
-import { getComponentClassNames } from "@marketplace/ui/namespace";
+import { AppointmentsCalendar } from "@marketplace/features/appointments-calendar";
 import { Metadata } from "next";
 import { redirect } from "next/navigation";
-import "./page.scss";
-
-const classes = getComponentClassNames("appointments-page", {
-  title: "title",
-  formContainer: "form-container",
-});
 
 const AppointmentsPage = async () => {
   const session = await authService.getSessionOrRedirect("/app/sobrecupos");
@@ -23,19 +14,13 @@ const AppointmentsPage = async () => {
     redirect("/app/perfil");
   }
 
-  const schedule = await appointmentsService.getSchedule(practitioner.id);
-
   return (
-    <div className={classes.namespace}>
-      <h1 className={classes.title}>Mis Sobrecupos</h1>
-      <Card className={classes.formContainer}>
-        <AppointmentsForm
-          specialtyCode={practitioner.specialty.code}
-          practitionerId={practitioner.id}
-          schedule={schedule}
-          practices={practitioner.practices}
-        />
-      </Card>
+    <div style={{ maxWidth: "1200px", margin: "0 auto", padding: "16px" }}>
+      <AppointmentsCalendar
+        practitionerId={practitioner.id}
+        specialtyCode={practitioner.specialty.code}
+        practices={practitioner.practices}
+      />
     </div>
   );
 };
@@ -47,7 +32,7 @@ export const metadata: Metadata = {
     follow: true,
     nocache: true,
     googleBot: {
-      index: true,
+      index: false,
       follow: false,
       noimageindex: true,
       "max-video-preview": -1,
