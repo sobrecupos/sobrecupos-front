d807efe57585a192b1e82ae9c65f1301abfc46d6
nullPointer.Ross <mauriciorossdev@gmail.com>
2024-04-11 04:12:26 -0400

    fix: fix error on deploy page

--- src/app/app/(admin)/dashboard/page.tsx
+++ src/app/app/(admin)/dashboard/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const hoverLink = "hover:cursor-pointer hover:shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]";
 
 
-export const DashboardPage = async () => {
+const DashboardPage = async () => {
     await authService.getAdminSessionOrRedirect();
 
     return (
