e40824ae102a6a3b271be587c7363ca66cac77f9
nullPointer.Ross <mauriciorossdev@gmail.com>
2024-04-15 16:14:52 -0400

    fix: add logs, add 4 hours

--- src/ui/ListDays/ListDays.tsx
+++ src/ui/ListDays/ListDays.tsx
@@ -26,9 +26,7 @@ export const ListDays = (
     from,
     to
   }: ListDaysProps) => {
-  console.log('from', from)
   const startDate = dayjs(from).add(4, 'hour').toDate();
-  console.log('startDate', startDate)
   const endDate = dayjs(to).toDate();
   const [IndexDaySelected, setIndexDaySelected] = useState<number>(indexDaySelected || -1)
   const [Days, setDays] = useState<string[]>([])
